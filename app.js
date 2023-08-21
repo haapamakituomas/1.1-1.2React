18a4093615b9c33489919c108fdf8bda2b7c660e
haapamakituomas <115136792+haapamakituomas@users.noreply.github.com>
2023-08-21 13:13:27 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={parts[0].name} exercises1={parts[0].exercises}
+        part2={parts[1].name} exercises2={parts[1].exercises}
+        part3={parts[2].name} exercises3={parts[2].exercises}
+      />
+      <Total total={totalExercises} />
+    </div>
+  );
+};
+
+
+export default App;
